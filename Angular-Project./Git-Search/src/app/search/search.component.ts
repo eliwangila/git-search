992b231caf2f8e697ab684e4cb800a362140f986
eliwangila <ekirapaeli254@gmail.com>
2021-08-27 15:48:53 +0300

    service

--- Angular-Project./Git-Search/src/app/search/search.component.ts
+++ Angular-Project./Git-Search/src/app/search/search.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
 
 @Component({
   selector: 'app-search',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
+  public userQuery!: string;
+  @Output() searchResult = new EventEmitter<any>()
   constructor() { }
+
+
 
   ngOnInit(): void {
   }
